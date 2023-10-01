bcc56d499cdd67facf423b07359b9ebad5ad3d9e
prakhar-kt <rahkarp@gmail.com>
2023-10-01 13:11:16 +0530

    added get route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+use rocket::serde::json::{Value, json};
+
+#[get("/")]
+fn hello() -> Value {
+    json!("Hello world")
+} 
+
+#[rocket::main] 
+async fn main() {
+    let _ = rocket::build()
+            .mount("/", routes![hello])
+            .launch()
+            .await;
 }
+
+
+
